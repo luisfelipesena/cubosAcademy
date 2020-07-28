0bea36a89f75aa173fa90aad2f9d3e0177312a45
Luis Felipe Cordeiro Sena <61807189+luisfelipesena@users.noreply.github.com>
2020-07-28 02:51:39 -0300

    Create ordenando_numeros_loop.js

---
+++ Lógica_de_Programação/ordenando_numeros_loop.js
@@ -0,0 +1,26 @@
+const numeros = [2, 5, 3, 0, 8,-1];
+let aux = [];
+let j = numeros.length; 
+let x;
+
+for (let h = 0; h < j; h++)
+{
+  for (x = 0; x < j - 1; x++)
+  {
+    /* x+1 necessita ficar primeiro que x nesse caso, logo cria-se
+     um vetor auxiliar para guardar um valor e trocar pelo outro*/
+    if (numeros[x] < numeros[x+1])
+    {
+      aux = numeros[x];
+      numeros[x] = numeros[x+1];
+      numeros[x+1] = aux;
+      x = -1;
+    }
+    else
+    {
+      continue;
+    }
+  }
+}
+  
+console.log(numeros);
