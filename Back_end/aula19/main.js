ce6d0b09ce31c6b748e84482d4d7348c7b959d4a
Luis Felipe <luisfelipesena@gmail.com>
2020-08-28 16:58:31 -0300

    funcionalidade: add + interatividade

--- Back_end/aula19/main.js
+++ Back_end/aula19/main.js
@@ -1,39 +1,41 @@
 const helpers = require("./helpers"); //Módulo com as funções de Suporte
 const fs = require("fs"); //Para escrever um novo Arquivo
 
-let registroMovimentacoes = []; //Array que guardará o registro de movimentações financeiras de certo correntista
 const correntistas = []; //Array que guardará todos os objetos dos correntistas
 
 //exemplos iniciais de correntistas que podem ser adicionados, utilizando as funções de suporte
 correntistas.push (
     {
     nome: "José",
-    cpf: helpers.cpf(helpers.retirarCaracteresNaoNumerais("66666666666")),
-    idBanco: helpers.idBanco(helpers.retirarCaracteresNaoNumerais("033")),
-    agencia: `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais("55555"))})`,
-    contaCorrente: helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais("7777777")),
-    saldo: 300000
+    cpf: helpers.cpf("66666666666"),
+    idBanco: helpers.idBanco("033"),
+    agencia: `(${helpers.agencia("55555")})`,
+    contaCorrente: helpers.contaCorrente("7777777"),
+    saldo: 300000,
+    registroMovimentacoes : []
     },
     {
     nome: "Elias",
-    cpf: helpers.cpf(helpers.retirarCaracteresNaoNumerais("55555555555")),
-    idBanco: helpers.idBanco(helpers.retirarCaracteresNaoNumerais("033")),
-    agencia: `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais("55565"))})`,
-    contaCorrente: helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais("7775577")),
-    saldo: 20000
+    cpf: helpers.cpf("55555555555"),
+    idBanco: helpers.idBanco("033"),
+    agencia: `(${helpers.agencia("55565")})`,
+    contaCorrente: helpers.contaCorrente("7775577"),
+    saldo: 20000,
+    registroMovimentacoes : []
     },
     {
     nome: "José",
-    cpf: helpers.cpf(helpers.retirarCaracteresNaoNumerais("66666666666")),
-    idBanco: helpers.idBanco(helpers.retirarCaracteresNaoNumerais("001")),
-    agencia: `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais("42565"))})`,
-    contaCorrente: helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais("8075577")),
-    saldo: 72450 
+    cpf: helpers.cpf("66666666666"),
+    idBanco: helpers.idBanco("001"),
+    agencia: `(${helpers.agencia("42565")})`,
+    contaCorrente: helpers.contaCorrente("8075577"),
+    saldo: 72450,
+    registroMovimentacoes : []
     }
 
 );
 
-//Função Opcional Caso Queira Imprimir dados de certo Correntista
+//Função *Opcional* Caso Queira Imprimir dados de certo Correntista expecífico em uma das funções
 const imprimirCorrentista = (correntista) => {
     console.log(`\nNome do Correntista: ${correntista.nome}`);
     console.log(`Cpf: ${correntista.cpf}`);
@@ -41,7 +43,6 @@ const imprimirCorrentista = (correntista) => {
     console.log(`Id do Banco: ${correntista.idBanco}`);
     console.log(`Agência: ${correntista.agencia}`);
     console.log(`Conta corrente: ${correntista.contaCorrente}`);
-    console.log(`Saldo: R$ ${(correntista.saldo / 100)}\n`);
 }
 
 //Função que analisa se já existe um correntista com esses dados, caso não, permite a criação de um novo
@@ -77,93 +78,156 @@ const procurarCorrentista = (cpf,idBanco) => {
 }
 
 //Funçao que adiciona no array de correntistas um novo caso possua dados diferentes dos existentes
-const addNovoCorrentista = (nome,cpf,idBanco,agencia,contaCorrente,saldo) => {
-    if (!analisarNovoCorrentista(cpf,idBanco,agencia,contaCorrente)) {
-        if (helpers.cpf(cpf)!= null && helpers.idBanco(idBanco) != null && 
-            helpers.agencia(agencia) != null && helpers.contaCorrente(contaCorrente) != null) {   
-                correntistas.push ({
-                    nome: nome,
-                    cpf: helpers.cpf(helpers.retirarCaracteresNaoNumerais(cpf)),
-                    idBanco: helpers.idBanco(helpers.retirarCaracteresNaoNumerais(idBanco)),
-                    agencia: `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais(agencia))})`,
-                    contaCorrente: helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais(contaCorrente)),
-                    saldo: saldo
-                });
-        }
-                            
-        else {
-            console.log("Digite Dados Válidos");
-            return false;
-        }
-                          
-    }
+const addNovoCorrentista = () => {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-    else  {
-        console.log("CPF já cadastrado.");
-        return false;
-    }
-                 
+    let nome; let cpf; let idBanco; let agencia; let contaCorrente; let saldo;
+    
+    rl.question("Qual o Nome do correntista ? " ,(nomeResp) => {
+        nome = nomeResp;
+        rl.question("Qual o cpf [11 dígitos] ? ", (cpfResp) => {
+            cpf = cpfResp;
+            rl.question("Qual o número do Banco [3 dígitos] ? ", (idBancoResp) => {
+                idBanco = idBancoResp;
+                rl.question("Qual a agência [5 dígitos] ? ", (agenciaResp) => {
+                    agencia = agenciaResp;
+                    rl.question("Qual a conta corrente [7 dígitos] ? ", (contaCorrenteResp) => {
+                        contaCorrente = contaCorrenteResp;
+                        saldo = 0 //saldo inicial
+                        if (!analisarNovoCorrentista(cpf,idBanco,agencia,contaCorrente)) {
+                            if (helpers.cpf(cpf)!= null && helpers.idBanco(idBanco) != null && 
+                                helpers.agencia(agencia) != null && helpers.contaCorrente(contaCorrente) != null) {   
+                                    correntistas.push ({
+                                        nome: nome,
+                                        cpf: helpers.cpf(helpers.retirarCaracteresNaoNumerais(cpf)),
+                                        idBanco: helpers.idBanco(helpers.retirarCaracteresNaoNumerais(idBanco)),
+                                        agencia: `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais(agencia))})`,
+                                        contaCorrente: helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais(contaCorrente)),
+                                        saldo: saldo
+                                    });
+                                    console.log(correntistas.splice(-1)); //imprime o adicionado
+                            }
+                                                
+                            else {
+                                console.log("Digite Dados Válidos");
+                            }
+                                              
+                        }
+                    
+                        else  {
+                            console.log("CPF já cadastrado.");
+                        }
+                        rl.close();
+                    })
+                })
+            })
+        });
+    });
+              
 }
 
 //Função que atualiza os dados de algum correntista, menos o saldo e id do Banco
-const atualizarCorrentista = (cpf,idBanco,propriedade,valorPropriedade) => {
-    if(!procurarCorrentista(cpf,idBanco)) {
-        return false;
-    }
+const atualizarCorrentista = () => {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-    else {
-        let correntista = procurarCorrentista(cpf,idBanco);
-        if (propriedade.includes("saldo") || propriedade.includes("idBanco")) {
-                console.log("Não pode editar essa propriedade");
-        }
-
-        else {
-            if ((propriedade == "nome" &&valorPropriedade != null) || 
-                helpers.cpf(valorPropriedade)!= null ||
-                helpers.idBanco(valorPropriedade) != null || 
-                helpers.agencia(valorPropriedade) != null ||
-                helpers.contaCorrente(valorPropriedade) != null) {
-                    if (propriedade.includes("nome")){
-                        correntista.nome = valorPropriedade;
+    let cpf; let idBanco; let propriedade; let valorPropriedade
+    rl.question("Qual o cpf [11 dígitos] de quem deseja atualizar ? ", (cpfResp) => {
+        cpf = cpfResp;
+        rl.question("Qual o número do Banco [3 dígitos] ? ", (idBancoResp) => {
+            idBanco = idBancoResp;
+            let correntista = procurarCorrentista(cpf,idBanco);
+            rl.question(`Qual a propriedade de ${correntista.nome} deseja ser modificada ? `, (propriedadeResp) => {
+                propriedade = propriedadeResp;
+                rl.question(`Qual o valor dessa propriedade ${propriedade} ? `, (valorPropriedadeResp) => {
+                    valorPropriedade = valorPropriedadeResp;
+                    if(!procurarCorrentista(cpf,idBanco)) {
+                        rl.close();
                     }
-
-                    else if (propriedade.includes("cpf")) {
-                        correntista.cpf = helpers.cpf(helpers.retirarCaracteresNaoNumerais(valorPropriedade));
-                    }
-
-                    else if (propriedade.includes("agencia")) {
-                        correntista.agencia = `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais(valorPropriedade))})`;
-                    }
-                    
-                    else if (propriedade.includes("contaCorrente")) {
-                        correntista.contaCorrente = helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais(valorPropriedade));
-                    }
-
+                
                     else {
-                        console.log("Digite uma Propriedade Válida");
-                        return false;
+                        
+                        if (propriedade.includes("saldo") || propriedade.includes("idBanco")) {
+                                console.log("Não pode editar essa propriedade");
+                        }
+                
+                        else {
+                            if ((propriedade == "nome" &&valorPropriedade != null) || 
+                                helpers.cpf(valorPropriedade)!= null ||
+                                helpers.idBanco(valorPropriedade) != null || 
+                                helpers.agencia(valorPropriedade) != null ||
+                                helpers.contaCorrente(valorPropriedade) != null) {
+                                    if (propriedade.includes("nome")){
+                                        correntista.nome = valorPropriedade;
+                                    }
+                
+                                    else if (propriedade.includes("cpf")) {
+                                        correntista.cpf = helpers.cpf(helpers.retirarCaracteresNaoNumerais(valorPropriedade));
+                                    }
+                
+                                    else if (propriedade.includes("agencia")) {
+                                        correntista.agencia = `(${helpers.agencia(helpers.retirarCaracteresNaoNumerais(valorPropriedade))})`;
+                                    }
+                                    
+                                    else if (propriedade.includes("contaCorrente")) {
+                                        correntista.contaCorrente = helpers.contaCorrente(helpers.retirarCaracteresNaoNumerais(valorPropriedade));
+                                    }
+                
+                                    else {
+                                        console.log("Digite uma Propriedade Válida");
+                                        return false;
+                                    }
+                                    console.log(correntista);
+                            }
+                
+                            else {
+                                console.log("Digite Dados Válidos");
+                                return false;
+                            }
+                        }
+                        rl.close();
                     }
-            }
+                })
+            })
+        })
+    })
 
-            else {
-                console.log("Digite Dados Válidos");
-                return false;
-            }
-        }
-    }
 }
 
 //Função que apaga os registros de algum correntista caso exista
-const removerCorrentista = (cpf,idBanco) => {
-    if(!procurarCorrentista(cpf,idBanco)) {
-        return false;
-    }
+const removerCorrentista = () => {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-    else {
-        let correntista = procurarCorrentista(cpf,idBanco);
-        let indexCorrentista = correntistas.indexOf(correntista);
-        correntistas.splice(indexCorrentista,1);
-    }
+    let cpf; let idBanco;
+
+    rl.question("Qual o cpf de quem deseja remover [11 dígitos] ? ",(cpfResp) => {
+        cpf = cpfResp;
+        rl.question("Qual o número do banco [3 dígitos] ? ", (idBancoResp) => {
+            idBanco = idBancoResp;
+            if(!procurarCorrentista(cpf,idBanco)) {
+                rl.close();
+            }
+        
+            else {
+                let correntista = procurarCorrentista(cpf,idBanco);
+                let indexCorrentista = correntistas.indexOf(correntista);
+                console.log(`${correntista.nome} do ${helpers.nomeBanco(idBanco)}, removido com sucesso do Banco de Dados`);
+                correntistas.splice(indexCorrentista,1);
+                rl.close();
+            }
+        })
+    });
 }
 
 //Função que serve para depósito e qualquer entrada de dinheiro de um certo correntista existente e add a movimentação ao array
@@ -175,7 +239,7 @@ const addDepositoCorrentista = (cpf,idBanco,valorASerDepositado) => {
     else {
         let correntista = procurarCorrentista(cpf,idBanco);
         correntista.saldo += (valorASerDepositado * 100);
-        registroMovimentacoes.push({
+        correntista.registroMovimentacoes.push({
             transacao: "Realizada",
             cpf: cpf,
             idBanco: idBanco,
@@ -197,7 +261,7 @@ const removerDepositoCorrentista = (cpf,idBanco,valorASerRetirado) => {
         let correntista = procurarCorrentista(cpf,idBanco);
         if (correntista.saldo >= valorASerRetirado * 100) {
             correntista.saldo -= (valorASerRetirado * 100);
-            registroMovimentacoes.push({
+            correntista.registroMovimentacoes.push({
                 transacao: "Realizada",
                 cpf: cpf,
                 idBanco: idBanco,
@@ -208,7 +272,7 @@ const removerDepositoCorrentista = (cpf,idBanco,valorASerRetirado) => {
         }
         else {
             console.log("\nNão há saldo suficiente");
-            registroMovimentacoes.push({
+            correntista.registroMovimentacoes.push({
                 transacao: "Não Realizada",
                 cpf: cpf,
                 idBanco: idBanco,
@@ -233,9 +297,15 @@ const depositoCorrentistas = (cpfPagador,idBancoPagador,cpfReceptor,idBancoRecep
         removerDepositoCorrentista(cpfPagador,idBancoPagador,valorTransferido);
         addDepositoCorrentista(cpfReceptor,idBancoReceptor,valorTransferido);
         //se quiser imprimir os dados de quem pagou
-        // imprimirCorrentista(correntistaPagador); 
+        console.log("---------------------------------------------");
+        console.log("Pagador: ");
+        imprimirCorrentista(correntistaPagador); 
+        console.log(`- R$ ${valorTransferido}\n`);
         //se quiser imprimir os dados de quem recebeu
-        // imprimirCorrentista(correntistaReceptor); 
+        console.log("Destinatário: ");
+        imprimirCorrentista(correntistaReceptor); 
+        console.log(`+ R$ ${valorTransferido}`);
+        console.log("---------------------------------------------");
     }
 }
 
@@ -243,6 +313,7 @@ const depositoCorrentistas = (cpfPagador,idBancoPagador,cpfReceptor,idBancoRecep
 
 //Função que analisa as movimentações financeiras e a quantidade selecionada dessas de um correntista existente
 const extratoCpf = (cpf,idBanco,quantidadeRegistros) => {
+    let correntista = procurarCorrentista(cpf,idBanco);
     let extratoUser = [];
     let movimentacoesSelecionadas = [];
     if(!procurarCorrentista(cpf,idBanco)) {
@@ -250,7 +321,7 @@ const extratoCpf = (cpf,idBanco,quantidadeRegistros) => {
     }
 
     else {
-        registroMovimentacoes.forEach(item => {
+        correntista.registroMovimentacoes.forEach(item => {
             if (item.cpf == cpf && item.idBanco == idBanco) {
                 extratoUser.push(`${item.transacao} | ${item.tipo} | ${item.data} | R$ ${item.valor},00`);
             }
@@ -266,7 +337,7 @@ const extratoCpf = (cpf,idBanco,quantidadeRegistros) => {
 //Funcão que cria a frase que será impressa no arquivo extrato
 const formatarExtrato = (funcaoExtrato) => {
     if (!funcaoExtrato) {
-        console.log("Não há o que imprimir\n");
+        console.log("Não houve movimentações\n");
         return false;
     }
 
@@ -297,6 +368,7 @@ const formatarExtrato = (funcaoExtrato) => {
             }
             frase += (`${informacoes[i]}\n`);
         });
+        frase += `Saldo Atual: R$ ${correntista.saldo / 100}`;
         return [cpf,frase];
     }
 }
@@ -314,14 +386,9 @@ const novoArquivo = (formatarExtrato) => {
 }
 
 //Exemplos De Teste das Funções
-console.log(correntistas);
-addNovoCorrentista("Luis Felipe","11111111111","033","77777","3333333",500000);
-console.log(correntistas);
-atualizarCorrentista("11111111111","033","nome","Luis Felipe Cordeiro Sena");
-console.log(correntistas);
-removerCorrentista("55555555555","033");
-console.log(correntistas);
-depositoCorrentistas("11111111111","033","66666666666","033",70);
-depositoCorrentistas("11111111111","033","66666666666","001",70);
-console.log(correntistas);
-novoArquivo(formatarExtrato(extratoCpf("11111111111","033",2)));
+depositoCorrentistas("66666666666","033","55555555555","033",140); //transferência
+addDepositoCorrentista("66666666666","033",100); //Depósito
+removerDepositoCorrentista("55555555555","033",50); //saque 
+depositoCorrentistas("55555555555","033","66666666666","033",240); //transferência
+novoArquivo(formatarExtrato(extratoCpf("66666666666","033",3)));
+novoArquivo(formatarExtrato(extratoCpf("55555555555","033",3)));
