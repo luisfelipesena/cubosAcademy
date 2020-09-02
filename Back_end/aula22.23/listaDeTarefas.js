0033f10a5b6f247e82272c7c5238ac0edd15d804
Luis Felipe <luisfelipesena@gmail.com>
2020-09-02 02:11:01 -0300

    documentação: aula22.23

---
+++ Back_end/aula22.23/listaDeTarefas.js
@@ -0,0 +1,160 @@
+const Koa = require("koa");
+const server = new Koa();
+const bodyparser = require("koa-bodyparser");
+const { request } = require("http");
+
+server.use(bodyparser()); // add no ctx.request.body as infos provinientes do body
+
+const tarefa = {
+    titulo: 'Preparar Aula',
+    descricao : 'Aula dia 21',
+    concluida: false,
+    deletado: false
+}
+
+const listaDeTarefas = [tarefa];
+
+const obterListaDeTarefas = () => {
+    const listaSemDeletados = [];
+    listaDeTarefas.forEach(item => {
+        if(!item.deletado) {
+            listaSemDeletados.push(item);
+        }
+    });
+    return listaSemDeletados;
+}
+
+const adicionarTarefa = (tarefa) => {
+    const novaTarefa = {
+        titulo: tarefa.titulo? tarefa.titulo :'Tarefa sem Título',
+        descricao: tarefa.descricao? tarefa.descricao :'Descrição sem Título',
+        concluida: tarefa.concluida? tarefa.concluida :false,
+        deletada: false
+    };
+    listaDeTarefas.push(novaTarefa);
+    return novaTarefa;
+}
+
+const obterTarefa = (index) => {
+    const tarefa = listaDeTarefas[index];
+    if (tarefa) {
+        return tarefa;
+    }
+    else {
+        return null;
+    }
+}
+
+const deletarTarefa = (index) => {
+    const tarefa = listaDeTarefas[index];
+    if (tarefa) {
+        listaDeTarefas.splice(index,1);
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+const atualizarConclusaoTarefa = (index,estado) => {
+    const tarefa = obterTarefa(index);
+    if (tarefa) {
+        const tarefaAtualizada = {
+            titulo: tarefa.titulo,
+            descricao: tarefa.descricao,
+            concluida: estado,
+            deletado: tarefa.deletado,
+        };
+
+        listaDeTarefas.splice(index,1,tarefaAtualizada);
+        return tarefaAtualizada;
+    }
+    else {
+        return false;
+    }
+    
+}
+
+
+const contexto = async (ctx) => {
+    const path = ctx.url;
+    const method = ctx.method;
+    if (path === '/tarefas') {
+
+        if (method === "GET") { //listar tarefas
+            ctx.body = obterListaDeTarefas();
+        }
+
+        else if (method == "POST") {
+            const novaTarefa = ctx.request.body;
+            const tarefa = adicionarTarefa(novaTarefa);
+            ctx.body = tarefa;
+        }
+
+        else {
+            ctx.status = 404;
+            ctx.body = 'Não Encontrado';
+        }
+        
+    }
+    
+    else if (path.includes("/tarefas/")) { 
+        const paths = path.split("/");
+        if (paths[1] === "tarefas") {
+            const index = paths[2];
+            if (index) {
+                if (method === "GET") {
+                    ctx.body = obterTarefa(index); 
+                }
+        
+                else if (method === "DELETE") {
+                    const resposta = deletarTarefa(index);
+                    if (resposta) {
+                        ctx.body = "Tarefa Deletada";
+                    }
+                    else {
+                        ctx.body = "Não foi possível deletar a tarefa";
+                    }
+                }
+
+                else if (method == "PUT") {
+                    const estado = ctx.request.body.estado;
+                    if (index && estado == true && estado == false) {
+                        ctx.status = 400;
+                        ctx.body = "Requisição mal formatada";
+                    }
+        
+                    else {
+                        ctx.body = "Tarefa Concluida";
+                        const resposta = atualizarConclusaoTarefa(index,estado);
+                        if (resposta) {
+                            ctx.body = resposta;
+                        }
+                        else {
+                            ctx.status = 404;
+                            ctx.body = "Não foi possivel atualizar";
+                        }
+                    }
+                }
+
+                else {
+                    ctx.status = 404;
+                    ctx.body = "Não encontrado";
+                } 
+            }       
+        }
+    }
+
+
+    else {
+        ctx.status = 404;
+        ctx.body = "Não encontrado";
+    }
+}
+
+server.use(contexto);
+
+server.listen(8081,() => {
+    console.log("O N L I N E !");
+})
---
+++ Back_end/aula22.23/script.js
@@ -0,0 +1,274 @@
+const Koa = require("koa");
+const server = new Koa();
+const bodyparser = require ("koa-bodyparser");
+server.use(bodyparser());
+
+//CLASSE
+//Questão 0 
+// const contexto = async (ctx) => {
+//     ctx.body = "Hello World!";
+// }
+
+// server.use(contexto);
+
+// server.listen(8081, () => {
+//     console.log("Servidor ta ON!");
+// })
+
+//Questão 1
+//correntistas será usado durante o código
+let correntistas = [];
+correntistas.push({
+    nome: 'Luis Felipe',
+    idade: 18,
+    cpf: "86394430501",
+    saldo: 500000
+});
+
+
+// const contexto = async (ctx) => {
+//     const path = ctx.url;
+//     const method = ctx.method;
+//     if (path == "/correntistas") {
+
+//         if (method === "GET") {
+//             ctx.body = correntistas;
+//         }
+
+//         else {
+//             ctx.status = 404;
+//             ctx.body = "Não Encontrada";
+//         }
+//     }
+
+//     else {
+//         ctx.status = 404;
+//         ctx.body = "Não Encontrada";
+//     }     
+// }
+
+// server.use(contexto);
+
+// server.listen(8081, () => {
+//     console.log("Servidor ta ON!");
+// })
+
+//Questão 2
+// const adicionarCorrentista = (correntista) => {
+//     const correntistaFormatado = {
+//         nome: (correntista.nome)? correntista.nome :null,
+//         idade: (correntista.idade)? correntista.idade :null,
+//         cpf: (correntista.cpf)? (correntista.cpf.length == 11)? correntista.cpf :"Cpf Incompleto" :null,
+//         saldo: (correntista.saldo)? correntista.saldo :null,
+//     }
+
+//     for (let i = 0; i < correntistas.length; i++) {
+//         if (correntistas[i].cpf == correntistaFormatado.cpf) {
+//             return false;
+//         }
+//     }
+    
+//     return correntistaFormatado;
+       
+// }
+
+// const contexto = async (ctx) => {
+//     const path = ctx.url;
+//     const method = ctx.method;
+//     if (path == "/correntistas") {
+
+//         if (method === "GET") {
+//             ctx.body = correntistas;
+//         }
+
+//         else if (method == "POST") {
+//             const correntista = ctx.request.body;
+//             const resposta = adicionarCorrentista(correntista);
+//             if (!resposta) {
+//                 ctx.status = 404;
+//                 ctx.body = "Correntista já existente";
+//             }
+//             else {
+//                 ctx.body = resposta;
+//                 correntistas.push(resposta);
+//             }
+//         }
+
+//         else {
+//             ctx.status = 404;
+//             ctx.body = "Não Encontrada";
+//         }
+//     }
+    
+//     else {
+//         ctx.status = 404;
+//         ctx.body = "Não Encontrada";
+//     }     
+// }
+
+// server.use(contexto);
+
+// server.listen(8081, () => {
+//     console.log("Servidor ta ON!");
+// })
+
+//CASA
+//Questão 3 e 4 - Código Final
+const adicionarCorrentista = (correntista) => {
+    const correntistaFormatado = {
+        nome: (correntista.nome)? correntista.nome :null,
+        idade: (correntista.idade)? correntista.idade :null,
+        cpf: (correntista.cpf)? (correntista.cpf.length == 11)? correntista.cpf :"Cpf Incompleto" :null,
+        saldo: (correntista.saldo)? correntista.saldo :null,
+    }
+
+    for (let i = 0; i < correntistas.length; i++) {
+        if (correntistas[i].cpf == correntistaFormatado.cpf) {
+            return false;
+        }
+    }
+    
+    return correntistaFormatado;
+       
+}
+
+const procurarCorrentista = (index) => {
+    for (let i = 0; i < correntistas.length; i++) {
+        if (i == index) {
+            return correntistas[i];
+        }
+    }
+    
+    return null;
+}
+
+const atualizarCorrentista = (index,nome,idade,cpf) => {
+    const correntista = procurarCorrentista(index);
+    if (nome) {
+        correntista.nome = nome;
+    }
+
+    if (idade) {
+        correntista.idade = idade;
+    }
+
+    if (cpf) {
+        cpf = (cpf.length == 11)? cpf :"Cpf Incompleto",
+        correntista.cpf = cpf;
+    }
+
+    else if (cpf == null && nome == null && cpf == null) {
+        return null;
+    }
+
+    return correntista;
+   
+}
+
+const deletarCorrentista = (index) => {
+    correntistas.splice(index,1);
+}
+
+const contexto = async (ctx) => {
+    const path = ctx.url;
+    const method = ctx.method;
+    if (path == "/correntistas") {
+
+        if (method === "GET") {
+            ctx.body = correntistas;
+        }
+
+        else if (method == "POST") {
+            const correntista = ctx.request.body;
+            const resposta = adicionarCorrentista(correntista);
+            if (!resposta) {
+                ctx.status = 404;
+                ctx.body = "Correntista já existente";
+            }
+            else {
+                ctx.body = resposta;
+                correntistas.push(resposta);
+            }
+        }
+
+        else {
+            ctx.status = 404;
+            ctx.body = "Não Encontrada";
+        }
+    }
+    
+    else if (path.includes("/correntistas/")) {
+        const pathFormatado = path.split("/");
+        const id = pathFormatado[2];
+        if (id) {
+            if (method === "PUT") {
+                const resposta = procurarCorrentista(id);
+                if (resposta == null) {
+                    ctx.status = 404;
+                    ctx.body = "Não Encontrado";
+                }
+
+                else {
+                    const nome = ctx.request.body.nome;
+                    const idade = ctx.request.body.idade;
+                    const cpf = ctx.request.body.cpf;
+                    const correntistaAtualizado = atualizarCorrentista(id,nome,idade,cpf);
+                    if (correntistaAtualizado == null) {
+                        ctx.status = 404;
+                        ctx.body = "Atualização Incompleta";
+                    }
+
+                    else {
+                        ctx.body = correntistaAtualizado;
+                    }
+                }
+            }
+
+            else if (method == "DELETE") {
+                const resposta = procurarCorrentista(id);
+                if (resposta == null) {
+                    ctx.status = 404;
+                    ctx.body = "Não Encontrado";
+                }
+
+                else {
+                    deletarCorrentista(id);
+                    ctx.body = "Correntista deletado com sucesso";
+                }
+            }
+
+            else if (method == "GET") {
+                const resposta = procurarCorrentista(id);
+                if (resposta == null) {
+                    ctx.status = 404;
+                    ctx.body = "Não Encontrado";
+                }
+
+                else {
+                    ctx.body = resposta;
+                }
+            }
+           
+            else {
+                ctx.status = 404;
+                ctx.body = "Não Encontrado";
+            }
+        }
+
+        else {
+            ctx.status = 404;
+            ctx.body = "Id Não Encontrado";
+        }
+    }
+
+    else {
+        ctx.status = 404;
+        ctx.body = "Não Encontrada";
+    }     
+}
+
+server.use(contexto);
+
+server.listen(8081, () => {
+    console.log("Servidor ta ON!");
+})
