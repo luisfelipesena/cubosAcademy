4b526bce6a51f158531cd25207ae560180da4e12
Luis Felipe <luisfelipesena@gmail.com>
2020-09-02 14:08:14 -0300

    correção: add idBanco,agencia e conta nos correntistas

--- Back_end/aula22.23/main.js
+++ Back_end/aula22.23/main.js
@@ -15,119 +15,47 @@ server.use(bodyparser()); // add no ctx.request.body as infos provinientes do bo
 //     console.log("Servidor ta ON!");
 // })
 
-//Questão 1
-//correntistas será usado durante o código
+//CASA - Código Final
 let correntistas = [];
 correntistas.push({
     nome: 'Luis Felipe',
     idade: 18,
-    cpf: "86394430501",
+    idBanco: '104',
+    agencia: '32112',
+    conta: '1235467',
+    cpf: '86394430501',
     saldo: 500000
 });
 
-// const contexto = async (ctx) => {
-//     const path = ctx.url;
-//     const method = ctx.method;
-//     if (path == "/correntistas") {
-
-//         if (method === "GET") {
-//             ctx.body = correntistas;
-//         }
-
-//         else {
-//             ctx.status = 404;
-//             ctx.body = "Não Encontrada";
-//         }
-//     }
-
-//     else {
-//         ctx.status = 404;
-//         ctx.body = "Não Encontrada";
-//     }     
-// }
-
-// server.use(contexto);
-
-// server.listen(8081, () => {
-//     console.log("Servidor ta ON!");
-// })
-
-//Questão 2
-// const adicionarCorrentista = (correntista) => {
-//     const correntistaFormatado = {
-//         nome: (correntista.nome)? correntista.nome :null,
-//         idade: (correntista.idade)? correntista.idade :null,
-//         cpf: (correntista.cpf)? (correntista.cpf.length == 11)? correntista.cpf :"Cpf Incompleto" :null,
-//         saldo: (correntista.saldo)? correntista.saldo :null,
-//     }
-
-//     for (let i = 0; i < correntistas.length; i++) {
-//         if (correntistas[i].cpf == correntistaFormatado.cpf) {
-//             return false;
-//         }
-//     }
-    
-//     return correntistaFormatado;
-       
-// }
-
-// const contexto = async (ctx) => {
-//     const path = ctx.url;
-//     const method = ctx.method;
-//     if (path == "/correntistas") {
-
-//         if (method === "GET") {
-//             ctx.body = correntistas;
-//         }
-
-//         else if (method == "POST") {
-//             const correntista = ctx.request.body;
-//             const resposta = adicionarCorrentista(correntista);
-//             if (!resposta) {
-//                 ctx.status = 404;
-//                 ctx.body = "Correntista já existente";
-//             }
-//             else {
-//                 ctx.body = resposta;
-//                 correntistas.push(resposta);
-//             }
-//         }
-
-//         else {
-//             ctx.status = 404;
-//             ctx.body = "Não Encontrada";
-//         }
-//     }
-    
-//     else {
-//         ctx.status = 404;
-//         ctx.body = "Não Encontrada";
-//     }     
-// }
-
-// server.use(contexto);
-
-// server.listen(8081, () => {
-//     console.log("Servidor ta ON!");
-// })
-
-//CASA
-//Questão 3 e 4 - Código Final
 const adicionarCorrentista = (correntista) => {
-    const correntistaFormatado = {
-        nome: (correntista.nome)? correntista.nome :null,
-        idade: (correntista.idade)? correntista.idade :null,
-        cpf: (correntista.cpf)? (correntista.cpf.length == 11)? correntista.cpf :"Cpf Incompleto" :null,
-        saldo: (correntista.saldo)? correntista.saldo :null,
+    if (correntista.idBanco && correntista.agencia && correntista.conta && correntista.cpf) {
+        correntista.idBanco = correntista.idBanco.toString(); 
+        correntista.agencia = correntista.agencia.toString(); 
+        correntista.conta = correntista.conta.toString();
+        correntista.cpf = correntista.cpf.toString();
+
+        const correntistaFormatado = {
+            nome: (correntista.nome)? correntista.nome :null,
+            idade: (correntista.idade)? correntista.idade :null,
+            idBanco: (correntista.idBanco)? (correntista.idBanco.length == 3)? correntista.idBanco :"Número do Banco Incompleto" :null,
+            agencia: (correntista.agencia)? (correntista.agencia.length == 5)? correntista.agencia :"Número da Agência Incompleto" :null,
+            conta: (correntista.conta)? (correntista.conta.length == 7)? correntista.conta :"Número da Conta Incompleto" :null,
+            cpf: (correntista.cpf)? (correntista.cpf.length == 11)? correntista.cpf :"Cpf Incompleto" :null,
+            saldo: (correntista.saldo)? correntista.saldo :null,
+        }
+    
+        for (let i = 0; i < correntistas.length; i++) { //caso já exista
+            if (correntistas[i].cpf == correntistaFormatado.cpf && correntistas[i].idBanco == correntistaFormatado.idBanco) {
+                return false;
+            }
+        }
+        
+        return correntistaFormatado;
     }
 
-    for (let i = 0; i < correntistas.length; i++) {
-        if (correntistas[i].cpf == correntistaFormatado.cpf) {
-            return false;
-        }
+    else { //caso os dados estejam incompletos
+        return false;
     }
-    
-    return correntistaFormatado;
        
 }
 
@@ -141,7 +69,7 @@ const procurarCorrentista = (index) => {
     return null;
 }
 
-const atualizarCorrentista = (index,body) => { //saldo não pode ser modificado
+const atualizarCorrentista = (index,body) => { //nem saldo nem idBanco podem ser modificados
     const correntista = procurarCorrentista(index);
     if (body.nome) {
         correntista.nome = body.nome;
@@ -151,12 +79,25 @@ const atualizarCorrentista = (index,body) => { //saldo não pode ser modificado
         correntista.idade = body.idade;
     }
 
+    if (body.agencia) {
+        body.agencia = body.agencia.toString(); 
+        body.agencia = (body.agencia.length == 5)? body.agencia :"Número da Agência Incompleto";
+        correntista.agencia = body.agencia;
+    }
+
+    if (body.conta) {
+        body.conta = body.conta.toString();
+        body.conta = (body.conta.length == 7)? body.conta :"Número da Conta Incompleto";
+        correntista.conta = body.conta;
+    }
+
     if (body.cpf) {
-        body.cpf = (cpf.length == 11)? body.cpf :"Cpf Incompleto",
+        body.cpf = body.cpf.toString();
+        body.cpf = (body.cpf.length == 11)? body.cpf :"Cpf Incompleto";
         correntista.cpf = body.cpf;
     }
 
-    else if ((body.cpf == null || body.cpf == "") && (body.nome == null || body.nome == "") && (body.idade == null || body.idade == "")) {
+    else if (body.cpf == null  && body.nome == null  && body.idade == null && body.agencia == null && body.conta == null) {
         return null;
     }
 
@@ -182,7 +123,7 @@ const contexto = async (ctx) => {
             const resposta = adicionarCorrentista(correntista);
             if (!resposta) {
                 ctx.status = 400;
-                ctx.body = "Correntista já existente";
+                ctx.body = "Formulário mal formatado";
             }
             else {
                 ctx.body = resposta;
@@ -230,7 +171,7 @@ const contexto = async (ctx) => {
 
                 else {
                     deletarCorrentista(id);
-                    ctx.body = `Correntista: ${resposta.nome}, deletado com sucesso`; //Pode ser o CPF Também
+                    ctx.body = `Correntista ${resposta.nome} do Banco ${resposta.idBanco}, deletado com sucesso`; //Pode ser o CPF Também
                 }
             }
 
