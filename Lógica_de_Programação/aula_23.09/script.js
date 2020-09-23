42d2235ae0611e782d0966b6ef643bd0d2c79efa
Luis Felipe <luisfelipesena@gmail.com>
2020-09-23 20:52:59 -0300

    documentação:  aula_23.09

---
+++ Lógica_de_Programação/aula_23.09/script.js
@@ -0,0 +1,39 @@
+//Questao 1 -
+const arr1 = ['a', 'b', 'c'];
+const arr2 = ['d', 'e'];
+const juncao = [...arr1,...arr2];
+console.log(juncao);
+
+//Questao 2 -
+const arr1 = ['a', 'b', 'c'];
+const arr2 = ['d', 'e'];
+
+const juncao = ['z',...arr1,'letras',...arr2,'f'];
+console.log(juncao);
+
+//Questão 3 - 
+const carro = {
+    modelo: "Gol",
+    marca: "Volkswagen",
+    cor: "Vermelho",
+    potenciaCV: 80,
+    preco: 3000000
+};
+
+let {preco, ...carroSemPreco} = carro;
+console.log(carro);
+
+//Questão 4 e 5 e 6- 
+const ordenar = (...args) => {
+    for (let i = 0; i < args.length; i++) {
+        for (let x = 0; x < args.length - 1; x++) {
+            if (args[x] > args[x+1]) {
+                [args[x],args[x+1]] = [args[x+1],args[x]];
+            }
+        }
+    }
+    console.log(args);
+}
+
+let arrayInteiros = [-10, -4, 0, 50, 9];
+ordenar(...arrayInteiros);
