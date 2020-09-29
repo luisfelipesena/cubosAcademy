6e49cf40f60e1c566d7c73119e0e49ac7cc1f54d
Luis Felipe <luisfelipesena@gmail.com>
2020-09-28 21:32:54 -0300

    aula_28.09

--- Lógica_de_Programação/aula_28.09/script.js
+++ Lógica_de_Programação/aula_28.09/script.js
@@ -1,14 +1,14 @@
-//Exercícios Propostos
-//Questão 1
-const array = [
-  "Alo alo",
-  "Prazer estamos vivenciando uma nova era",
-  "Hello World",
-  "BBMP",
-];
+// //Exercícios Propostos
+// //Questão 1
+// const array = [
+//   "Alo alo",
+//   "Prazer estamos vivenciando uma nova era",
+//   "Hello World",
+//   "BBMP",
+// ];
 
-const encontrado = array.find((item) => item.length > 10);
-console.log(encontrado);
+// const encontrado = array.find((item) => item.length > 10);
+// console.log(encontrado);
 
 //Questão 2
 const texto = `Ola
@@ -19,55 +19,55 @@ const textoArray = texto.split("");
 const indices = [];
 let indexQuebrasLinha = 0;
 while (indexQuebrasLinha !== -1) {
-  indexQuebrasLinha = textoArray.findIndex((item) => item === "\n");
+  indexQuebrasLinha = textoArray.findIndex((item) => item === "\n"); //ou textoArray.indexOf("\n")
   if (indexQuebrasLinha !== -1) {
     indices.push(indexQuebrasLinha);
     textoArray.splice(indexQuebrasLinha, 1);
   }
-} //indexof("/n");
+}
 
 console.log(indices);
 
-//Questão 3
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const par = array.some((item) => item % 2 === 0);
-console.log(par);
+// //Questão 3
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const par = array.some((item) => item % 2 === 0);
+// console.log(par);
 
-//Questão 4
-const array = [1, 2, 3, 4, 5, 6, 15, 16, 35, 36];
+// //Questão 4
+// const array = [1, 2, 3, 4, 5, 6, 15, 16, 35, 36];
 
-const possuiQuadradoPerfeito = (numeros) => {
-  const quadradoPerfeito = numeros.some((item) => {
-    if (
-      item > 0 &&
-      Math.sqrt(item) > 0 &&
-      Math.pow(Math.sqrt(item), 2) === item &&
-      !Math.sqrt(item).toString().includes(".")
-    ) {
-      return true;
-    }
-  });
-  return quadradoPerfeito;
-};
+// const possuiQuadradoPerfeito = (numeros) => {
+//   const quadradoPerfeito = numeros.some((item) => {
+//     if (
+//       item > 0 &&
+//       Math.sqrt(item) > 0 &&
+//       Math.pow(Math.sqrt(item), 2) === item &&
+//       !Math.sqrt(item).toString().includes(".")
+//     ) {
+//       return true;
+//     }
+//   });
+//   return quadradoPerfeito;
+// };
 
-console.log(possuiQuadradoPerfeito(array));
+// console.log(possuiQuadradoPerfeito(array));
 
-//Questão 4 - método 2
-const array = [3, 4, 5];
-const possuiQuadradoPerfeito = (numeros) => {
-  const quadradoPerfeito = numeros.some((item) => {
-    for (let i = 0; i < item; i++) {
-      if (item === i * i) {
-        return true;
-      }
-    }
-  });
-  return quadradoPerfeito;
-};
+// //Questão 4 - método 2
+// const array = [3, 4, 5];
+// const possuiQuadradoPerfeito = (numeros) => {
+//   const quadradoPerfeito = numeros.some((item) => {
+//     for (let i = 0; i < item; i++) {
+//       if (item === i * i) {
+//         return true;
+//       }
+//     }
+//   });
+//   return quadradoPerfeito;
+// };
 
-console.log(possuiQuadradoPerfeito(array));
+// console.log(possuiQuadradoPerfeito(array));
 
-//Questão 5
-const array = [50, 4000, 5000, 7000, 9000, 100];
-const centavos = array.every((item) => item > 0 && Math.floor(item) === item);
-console.log(centavos);
+// //Questão 5
+// const array = [50, 4000, 5000, 7000, 9000, 100];
+// const centavos = array.every((item) => item > 0 && Math.floor(item) === item);
+// console.log(centavos);
